a6e080f7c5296c5b6bcf41a53f2cf0f889b12e88
sofik20 <zavalovasofa0@gmail.com>
2026-04-23 11:05:41 +0700

    Лабораторная №2-1

---
+++ backend/api/routes.py
@@ -0,0 +1,15 @@
+from services.status_service import get_status_data, get_items_data
+from schemas.status_schema import StatusOutSchema
+from schemas.item_schema import ItemsOutSchema
+
+
+def register_routes(app):
+    @app.get("/api/status")
+    @app.output(StatusOutSchema)
+    def api_status():
+        return get_status_data()
+
+    @app.get("/api/items")
+    @app.output(ItemsOutSchema)
+    def api_items():
+        return {"items": get_items_data()}
